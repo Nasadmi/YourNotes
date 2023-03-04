25e73b9d06915571f6a30955f93bea21c99fc94f
Nasadmi <samueldiego828@gmail.com>
2023-03-04 18:05:25 +0100

    modificados:     src/public/css/globalStyles.css 	modificados:     src/router/indexRoutes.js

--- src/router/indexRoutes.js
+++ src/router/indexRoutes.js
@@ -19,20 +19,34 @@ routes.get('/about', (req,res) => {
 })
 
 routes.get('/contact', (req,res) => {
-    res.render('c0ontact', {
+    res.render('contact', {
         title: process.env.TITLE
     })
 })
 
-routes.get('/', (req,res) => {
+routes.get('/login', (req,res) => {
     res.render('login', {
-        title: process.env.TITLE
+        title: process.env.TITLE,
+        alert: false,
+        alertTitle: undefined,
+        alertMessage: undefined,
+        imageUrl: undefined,
+        showConfirmButton: undefined,
+        timer: undefined,
+        route: undefined
     })
 })
 
 routes.get('/register', (req,res) => {
     res.render('register', {
-        title: process.env.TITLE
+        title: process.env.TITLE,
+        alert: false,
+        alertTitle: undefined,
+        alertMessage: undefined,
+        imageUrl: undefined,
+        showConfirmButton: undefined,
+        timer: undefined,
+        route: undefined
     })
 })
 
